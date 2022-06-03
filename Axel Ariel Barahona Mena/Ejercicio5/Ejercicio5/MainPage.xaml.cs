969d327a2316a92f3ab8c0eb4edb540941b48b7f
Axel-Barahona16 <7266@itescam.edu.mx>
2022-06-03 02:04:48 -0500

    mamoelsemestre

---
+++ Axel Ariel Barahona Mena/Ejercicio5/Ejercicio5/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio5
+{
+    public partial class MainPage : ContentPage
+    {
+        int contador;
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            contador++;
+            ((Button)sender).Text = "Presionado: " + contador + " veces!!";
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
